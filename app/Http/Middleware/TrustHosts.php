c0191d22baf2d7eb701198087f717a76d5bbb371
iamgap23 <tiebyadjipatrickangedesire.goprou@edu.ece.fr>
2024-01-11 02:00:37 +0000

    maj

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -3,10 +3,28 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
-
+use Illuminate\Http\Request;
 class TrustHosts extends Middleware
 {
 
+        /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
     /**
      * Get the host patterns that should be trusted.
      *
